88e1bc18bc97c008bdbdc4eaeab30b6082e0be2c
treil <p.treilhes@free.fr>
2026-04-19 21:40:25 +0200

    update jfxplace test with default beans

--- jfxplace.starters/jfxplace.core.starter.test/src/main/java/com/treilhes/jfxplace/test/JfxPlaceTest.java
+++ jfxplace.starters/jfxplace.core.starter.test/src/main/java/com/treilhes/jfxplace/test/JfxPlaceTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.test.context.BootstrapWith;
+import org.testfx.framework.junit5.ApplicationExtension;
 
 import com.treilhes.emc4j.test.Emc4jCoreContext;
 import com.treilhes.emc4j.test.Emc4jDefault;
@@ -21,14 +22,26 @@ import com.treilhes.emc4j.test.Emc4jExtension;
 import com.treilhes.emc4j.test.Emc4jExtension.Emc4jTestContextBootstrapper;
 import com.treilhes.emc4j.test.Emc4jSpringExtension;
 import com.treilhes.emc4j.test.Emc4jTest;
+import com.treilhes.jfxplace.core.api.application.ApplicationClassloader;
+import com.treilhes.jfxplace.core.api.fxom.subjects.FxomEvents;
 import com.treilhes.jfxplace.core.api.i18n.BundleProvider;
 import com.treilhes.jfxplace.core.api.i18n.I18N;
+import com.treilhes.jfxplace.core.api.javafx.internal.FxmlControllerBeanPostProcessor;
+import com.treilhes.jfxplace.core.api.javafx.internal.JfxAppPlatformImpl;
+import com.treilhes.jfxplace.core.api.subjects.ApplicationEvents;
+import com.treilhes.jfxplace.core.api.subjects.ApplicationInstanceEvents;
+import com.treilhes.jfxplace.core.api.subjects.DockManager;
+import com.treilhes.jfxplace.core.api.subjects.LifecyclePostProcessor;
+import com.treilhes.jfxplace.core.api.subjects.ViewManager;
+import com.treilhes.jfxplace.core.api.task.TaskService;
+import com.treilhes.jfxplace.core.api.ui.controller.dock.ViewController;
 import com.treilhes.jfxplace.test.JfxPlaceTest.I18NTestConfig;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @BootstrapWith(Emc4jTestContextBootstrapper.class)
 @ExtendWith({
+    ApplicationExtension.class,
     MockitoExtension.class,
     Emc4jSpringExtension.class,
     Emc4jExtension.class
@@ -36,11 +49,30 @@ import com.treilhes.jfxplace.test.JfxPlaceTest.I18NTestConfig;
 @Emc4jTest(
         defaultConfig = @Emc4jDefault(
                 classes = {
-                        I18NTestConfig.class
+                        I18NTestConfig.class,
+                        JfxAppPlatformImpl.class,
+                        LifecyclePostProcessor.class,
+
+                        // events
+                        ApplicationEvents.ApplicationEventsImpl.class,
+                        ApplicationInstanceEvents.ApplicationInstanceEventsImpl.class,
+                        FxomEvents.FxomEventsImpl.class,
+                        ViewManager.ViewManagerImpl.class,
+                        DockManager.DockManagerImpl.class,
+
+                        // JavaFX
+                        ApplicationClassloader.class,
+                        FxmlControllerBeanPostProcessor.class,
+                        //FxmlControllerBeanPostProcessor2.class,
+
+                        // services
+                        TaskService.class,
+
+                        //UI
+                        ViewController.class, //base ui for views
                 }
         )
 )
-@ExtendWith({})
 public @interface JfxPlaceTest {
 
     @AliasFor(annotation = Emc4jTest.class)
